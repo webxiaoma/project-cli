33b5352e52178a9ba593bbb4172bc437166fd0b2
webxiaoma <webxiaoma@qq.com>
2019-05-15 18:10:09 +0800

    eslint

---
+++ packages/cli/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: [6, 7, 8]
+  },
+  rules: {
+    //规则是关于最佳实践
+
+    
+    // 风格指南
+  }
+};
