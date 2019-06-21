2fb6bad054f6583b70b35f73b4ad091eb9dd29dd
webxiaoma <webxiaoma@qq.com>
2019-06-21 18:26:01 +0800

    packages

--- packages/babel/babel-preset-pro-vue/index.js
+++ packages/babel/babel-preset-pro-vue/index.js
@@ -6,10 +6,8 @@ module.exports = (context, options = {}) => {
 
   let envOpt = options["@babel/preset-env"] || {};
 
-  let presets = [[require('./node_modules/@babel/preset-env'), envOpt]];
-  let plugins = [
-    ["module:@babel/plugin-syntax-dynamic-import"]
-  ];
+  let presets = [['@babel/preset-env', envOpt]];
+  let plugins = [];
   
   return {
     presets,
